6bcd9cc239173b45ed3b11563045c19990e8ec66
shukuril <99889459+shukuril@users.noreply.github.com>
2024-05-16 21:30:28 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-// Selectors for cart elements
+// Cart
 let cartIcon = document.querySelector("#cart-icon");
 let cart = document.querySelector(".cart");
 let closeCart = document.querySelector("#close-cart");
+let tg = window.Telegram.WebApp;
 
 // Open Cart
 cartIcon.addEventListener("click", () => {
@@ -9,61 +10,112 @@ cartIcon.addEventListener("click", () => {
 });
 
 // Close Cart
-closeCart.addEventListener("click", () => {
+closeCart.onclick = () => {
     cart.classList.remove("active");
-});
+};
 
-// Wait for the DOM to load before running the ready function
-if (document.readyState === 'loading') {
+// Cart Working JS
+if (document.readyState == 'loading') {
     document.addEventListener("DOMContentLoaded", ready);
 } else {
     ready();
 }
 
+// Making Function
 function ready() {
-    // Add event listeners to cart remove buttons
-    let removeCartButtons = document.querySelectorAll(".cart-remove");
-    removeCartButtons.forEach(button => {
+    // Remove Item From Cart
+    var removeCartButtons = document.getElementsByClassName("cart-remove");
+    for (var i = 0; i < removeCartButtons.length; i++) {
+        var button = removeCartButtons[i];
         button.addEventListener("click", removeItemFromCart);
-    });
+    }
 
-    // Add event listeners to quantity inputs
-    let quantityInputs = document.querySelectorAll(".cart-quantity");
-    quantityInputs.forEach(input => {
+    // Quantity Changes
+    var quantityInputs = document.getElementsByClassName("cart-quantity");
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i];
         input.addEventListener("change", quantityChanged);
-    });
+    }
 
-    // Add event listeners to "add to cart" buttons
-    let addCartButtons = document.querySelectorAll('.add-cart');
-    addCartButtons.forEach(button => {
+    // Add To Cart
+    var addCart = document.getElementsByClassName('add-cart');
+    for (var i = 0; i < addCart.length; i++) {
+        var button = addCart[i];
         button.addEventListener("click", addItemToCart);
+    }
+
+    // Buy button Work
+    document
+        .getElementsByClassName("btn-buy")[0]
+        .addEventListener("click", openOrderForm);
+
+    // Submit Order
+    document.querySelector('#submit-order').addEventListener('click', function() {
+        var name = document.querySelector('#name').value;
+        var phoneNumber = document.querySelector('#phone').value;
+        var address = document.querySelector('#address').value;
+
+        // Проверяем, все ли обязательные поля заполнены
+        if (name.trim() === '' || phoneNumber.trim() === '' || address.trim() === '') {
+            alert("Please fill in all required fields (name, phone number, address) before submitting.");
+            return; // Отменяем отправку формы
+        }
+
+        console.log('Name:', name);
+        console.log('Phone Number:', phoneNumber);
+        console.log('Address:', address);
+
+        document.querySelector('#name').value = '';
+        document.querySelector('#phone').value = '';
+        document.querySelector('#address').value = '';
+
+        document.querySelector('.order-form').style.display = 'none';
     });
 
-    // Add event listener to the buy button
-    document.querySelector(".btn-buy").addEventListener("click", buyButtonClicked);
+    // Close Order Form
+    document.querySelector('#close-form').addEventListener('click', function() {
+        document.querySelector('.order-form').style.display = 'none';
+    });
 
-    // Add event listener to filter products by name
+    // Filter Products by Name
     document.getElementById('name-filter').addEventListener('change', function() {
-        let filterValue = this.value.toLowerCase();
-        let productBoxes = document.querySelectorAll('.product-box');
-        productBoxes.forEach(box => {
-            let productName = box.querySelector('.product-title').innerText.toLowerCase();
-            box.style.display = (filterValue === 'all' || productName === filterValue) ? 'block' : 'none';
+        var filterValue = this.value.toLowerCase();
+        var productBoxes = document.querySelectorAll('.product-box');
+        productBoxes.forEach(function(box) {
+            var productName = box.querySelector('.product-title').innerText.toLowerCase();
+            if (filterValue === 'all' || productName === filterValue) {
+                box.style.display = 'block';
+            } else {
+                box.style.display = 'none';
+            }
         });
+    });
+
+    // Add event listener for name filter change
+    document.getElementById('name-filter').addEventListener('change', function() {
+        var selectedName = this.value;
+        filterProductsByName(selectedName);
     });
 }
 
-// Handle the buy button click
+// Open Order Form
+function openOrderForm() {
+    document.querySelector('.order-form').style.display = 'block';
+}
+
+// Обработка нажатия кнопки покупки
 function buyButtonClicked() {
     let cartContent = document.querySelector(".cart-content");
     let items = cartContent.querySelectorAll('.cart-box');
 
+    // Проверка на пустую корзину
     if (items.length === 0) {
-        alert("Your cart is empty. Please add items to your cart before proceeding to checkout.");
+        alert("Ваша корзина пуста. Пожалуйста, добавьте товары в корзину перед оформлением заказа.");
         return;
     }
 
-    let message = "Order Details:\n";
+    // Формирование сообщения с деталями заказа
+    let message = "Детали заказа:\n";
     items.forEach(item => {
         let imgSrc = item.querySelector('.cart-img').src;
         let title = item.querySelector('.cart-product-title').innerText;
@@ -71,103 +123,116 @@ function buyButtonClicked() {
         let quantity = item.querySelector('.cart-quantity').value;
         let size = item.querySelector('.cart-size').innerText.replace('Size: ', '');
         let color = item.querySelector('.cart-color').innerText.replace('Color: ', '');
-        
 
-        message += `\n Image: ${imgSrc}\n  ${title}\n  Price: ${price}\n  Quantity: ${quantity}\n  Size: ${size}\n  Color: ${color}\n`;
+        message += `\nИзображение: ${imgSrc}\nНазвание: ${title}\nЦена: ${price}\nКоличество: ${quantity}\nРазмер: ${size}\nЦвет: ${color}\n`;
     });
+    const url = `https://food.bato.uz/ba.php?data=${message}`;
 
-    console.log(message);
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var fileContent = xhr.responseText;
+            console.log(fileContent);
+            // Use the file content in your JavaScript code
+        }
+    };
+    xhr.send();
 
-    // Clear the cart after purchase
-    while (cartContent.firstChild) {
-        cartContent.removeChild(cartContent.firstChild);
+    // Отправка данных в Telegram Web App
+    if (tg) {
+        tg.sendData(message);
+        // Очистка корзины после покупки
+        while (cartContent.firstChild) {
+            cartContent.removeChild(cartContent.firstChild);
+        }
+        updateTotal();
+    } else {
+        console.error("Telegram Web App недоступен.");
     }
-
-    updateTotal();
 }
 
-// Remove an item from the cart
+
+// Remove Items From Cart
 function removeItemFromCart(event) {
-    let buttonClicked = event.target;
-    buttonClicked.parentElement.remove();
+    var buttonClicked = event.target;
+    buttonClicked.parentNode.remove();
     updateTotal();
 }
-
-// Handle quantity changes
+// Quantity Changes
 function quantityChanged(event) {
-    let input = event.target;
+    var input = event.target;
     if (isNaN(input.value) || input.value <= 0) {
         input.value = 1;
     }
     updateTotal();
 }
 
-// Add an item to the cart
+// Add To Cart
 function addItemToCart(event) {
-    let button = event.target;
-    let shopProducts = button.parentElement;
-    let title = shopProducts.querySelector(".product-title").innerText;
-    let price = shopProducts.querySelector(".price").innerText;
-    let productImg = shopProducts.querySelector(".product-img").src;
-    let size = shopProducts.querySelector(".size-selector").value;
-    let color = shopProducts.querySelector(".color-selector").value;
-    let productId = shopProducts.dataset.productId;
+    var button = event.target;
+    var shopProducts = button.parentElement;
+    var title = shopProducts.querySelector(".product-title").innerText;
+    var price = shopProducts.querySelector(".price").innerText;
+    var productImg = shopProducts.querySelector(".product-img").src;
+    var size = shopProducts.querySelector(".size-selector").value;
+    var color = shopProducts.querySelector(".color-selector").value;
+    var productId = shopProducts.dataset.productId;
 
     addProductToCart(title, price, productImg, size, color, productId);
-    updateTotal();
+    updateTotal(); // Добавлено здесь
 }
 
-// Add a product to the cart
+// Add Product To Cart
 function addProductToCart(title, price, productImg, size, color, productId) {
-    let cartItems = document.querySelector(".cart-content");
-
-    // Create a unique key for each combination of productId, size, and color
-    let productKey = productId + '-' + size + '-' + color;
-    let cartItemsKeys = cartItems.getElementsByClassName("cart-product-key");
-
-    for (let i = 0; i < cartItemsKeys.length; i++) {
-        if (cartItemsKeys[i].innerText.trim() === productKey.trim()) {
-            alert("You have already added this item to cart with the same size and color");
+    var cartShopBox = document.createElement("div");
+    cartShopBox.classList.add("cart-box");
+    var cartItems = document.querySelector(".cart-content");
+    var cartItemsIds = cartItems.getElementsByClassName("cart-product-id");
+    for (var i = 0; i < cartItemsIds.length; i++) {
+        if (cartItemsIds[i].innerText.trim() === productId.trim()) {
+            alert("You have already added this item to cart");
             return;
         }
     }
 
-    let cartShopBox = document.createElement("div");
-    cartShopBox.classList.add("cart-box");
-
-    let cartBoxContent = `
-        <img src="${productImg}" alt="" class="cart-img">
-        <div class="detail-box">
-            <div class="cart-product-title">${title}</div>
-            <div class="cart-price">${price}</div>
-            <div class="cart-size">Size: ${size}</div>
-            <div class="cart-color">Color: ${color}</div>
-            <input type="number" value="1" class="cart-quantity">
-        </div>
-        <i class='bx bx-trash-alt cart-remove'></i>
-        <div class="cart-product-id" style="display: none;">${productId}</div>
-        <div class="cart-product-key" style="display: none;">${productKey}</div>
-    `;
-
+    var cartBoxContent = `
+                    <img src="${productImg}" alt="" class="cart-img">
+                    <div class="detail-box">
+                        <div class="cart-product-title">${title}</div>
+                        <div class="cart-price">${price}</div>
+                        <div class="cart-size">${size}</div>
+                        <div class="cart-color">${color}</div>
+                        <input type="number" value="1" class="cart-quantity">
+                    </div>
+                    <!-- Remove Cart -->
+                    <i class='bx bx-trash-alt cart-remove'></i>
+                    <div class="cart-product-id" style="display: none;">${productId}</div>
+`;
     cartShopBox.innerHTML = cartBoxContent;
     cartItems.append(cartShopBox);
 
-    // Add event listeners for the new cart item
-    cartShopBox.querySelector(".cart-remove").addEventListener("click", removeItemFromCart);
-    cartShopBox.querySelector('.cart-quantity').addEventListener("change", quantityChanged);
+    cartShopBox
+        .getElementsByClassName("cart-remove")[0]
+        .addEventListener("click", removeItemFromCart);
+
+    cartShopBox
+        .querySelector('.cart-quantity')
+        .addEventListener("change", quantityChanged);
 }
 
-// Update the total price in the cart
+// Update Total
 function updateTotal() {
-    let cartBoxes = document.querySelectorAll('.cart-box');
-    let total = 0;
-    cartBoxes.forEach(cartBox => {
-        let priceElement = cartBox.querySelector('.cart-price');
-        let quantityElement = cartBox.querySelector('.cart-quantity');
-        let price = parseFloat(priceElement.innerText.replace("sum ", "").replace(",", ""));
-        let quantity = quantityElement.value;
+    var cartBoxes = document.querySelectorAll('.cart-box');
+    var total = 0;
+    for (var i = 0; i < cartBoxes.length; i++) {
+        var cartBox = cartBoxes[i];
+        var priceElement = cartBox.querySelector('.cart-price');
+        var quantityElement = cartBox.querySelector('.cart-quantity');
+        var price = parseFloat(priceElement.innerText.replace("sum ", "").replace(",", ""));
+        var quantity = quantityElement.value;
         total += price * quantity;
-    });
+    }
     total = total.toFixed(3);
-    document.querySelector(".total-price").innerText = "sum " + total;
+    document.querySelector(".total-price").innerText = "sum " + total; // Обновляем значение общей суммы
 }
